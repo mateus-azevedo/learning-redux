a6a24b82bf1451c73da3b11da07fca286156fce2
Mateus Santos <m.santos.azeve@gmail.com>
2021-06-18 16:53:47 -0300

    Adicionado Video

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Video from "./components/Video";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
+      <Video />
       <Sidebar />
     </div>
   );
