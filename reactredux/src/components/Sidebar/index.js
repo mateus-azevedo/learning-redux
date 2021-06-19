400d8041e6aebfef677b71a2f3bb7abffc217075
Mateus Santos <m.santos.azeve@gmail.com>
2021-06-19 13:39:14 -0300

    De Class Component para Function Component

--- reactredux/src/components/Sidebar/index.js
+++ reactredux/src/components/Sidebar/index.js
@@ -1,22 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Sidebar extends Component {
-  render() {
-    const { modules } = this.state;
+const Sidebar = () => (
+  <aside>
+    {modules.map((module) => (
+      <div key={module.id}>
+        <strong>{module.title}</strong>
+        <ul>
+          {module.lessons.map((lesson) => (
+            <li key={lesson.id}>{lesson.title}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </aside>
+);
 
-    return (
-      <aside>
-        {modules.map((module) => (
-          <div key={module.id}>
-            <strong>{module.title}</strong>
-            <ul>
-              {module.lessons.map((lesson) => (
-                <li key={lesson.id}>{lesson.title}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </aside>
-    );
-  }
-}
+export default Sidebar;
