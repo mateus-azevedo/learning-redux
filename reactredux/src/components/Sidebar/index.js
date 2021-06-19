c3866855126a72b3b62aecdc8b2344e0ee6cabcd
Mateus Santos <m.santos.azeve@gmail.com>
2021-06-19 19:11:04 -0300

    Action toggleLesson

--- reactredux/src/components/Sidebar/index.js
+++ reactredux/src/components/Sidebar/index.js
@@ -4,14 +4,27 @@ import { connect } from "react-redux";
 // connect utiliza o conceito hocs (High Order Components), mas é um pattern do
 // react para compartilhar uma informação para algum component
 
-const Sidebar = ({ modules }) => (
+function toggleLesson(module, lesson) {
+  return {
+    type: "TOGGLE_LESSON",
+    module,
+    lesson,
+  };
+}
+
+const Sidebar = ({ modules, dispatch }) => (
   <aside>
     {modules.map((module) => (
       <div key={module.id}>
         <strong>{module.title}</strong>
         <ul>
           {module.lessons.map((lesson) => (
-            <li key={lesson.id}>{lesson.title}</li>
+            <li key={lesson.id}>
+              {lesson.title}
+              <button onClick={() => dispatch(toggleLesson(module, lesson))}>
+                Selecionar
+              </button>
+            </li>
           ))}
         </ul>
       </div>
--- reactredux/src/store/index.js
+++ reactredux/src/store/index.js
@@ -1,30 +1,40 @@
 import { createStore } from "redux";
 // createStore cria um estado global, onde fica armazenado boa parte das informações
 
-function reducer() {
+const INITIAL_STATE = {
+  activeLesson: {},
+  activeModule: {},
+  modules: [
+    {
+      id: 1,
+      title: "Iniciando com React",
+      lessons: [
+        { id: 1, title: "Primeira Aula" },
+        { id: 2, title: "Segunda Aula" },
+      ],
+    },
+    {
+      id: 2,
+      title: "Aprendendo Redux",
+      lessons: [
+        { id: 3, title: "Terceira Aula" },
+        { id: 4, title: "Quarta Aula" },
+      ],
+    },
+  ],
+};
+
+function reducer(state = INITIAL_STATE, action) {
   // retorna como o estado vai inicializar na aplicação
-  return {
-    activeLesson: null,
-    activeModule: null,
-    modules: [
-      {
-        id: 1,
-        title: "Iniciando com React",
-        lessons: [
-          { id: 1, title: "Primeira Aula" },
-          { id: 2, title: "Segunda Aula" },
-        ],
-      },
-      {
-        id: 2,
-        title: "Aprendendo Redux",
-        lessons: [
-          { id: 3, title: "Terceira Aula" },
-          { id: 4, title: "Quarta Aula" },
-        ],
-      },
-    ],
-  };
+  if (action.type === "TOGGLE_LESSON") {
+    return {
+      ...state,
+      activeLesson: action.lesson,
+      activeModule: action.module,
+    };
+  }
+
+  return state;
 }
 
 const store = createStore(reducer);
