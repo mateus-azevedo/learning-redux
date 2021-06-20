41711978d6fa54ee7cef83ee40184111ce0b7407
Mateus Santos <m.santos.azeve@gmail.com>
2021-06-20 15:15:16 -0300

    Redux incorporado ao projeto

--- algashopping-live-03/src/components/Calculator.jsx
+++ algashopping-live-03/src/components/Calculator.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { sum } from "../store/Calculator/Calculator.actions";
+import { sum, subtract } from "../store/Calculator/Calculator.actions";
 
 function Calculator() {
   const dispatch = useDispatch();
   const result = useSelector((state) => state.calculator);
-
-  //dispatch(sum)
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
 
   return (
     <>
-      <input type="text" placeholder="a" />
-      <input type="text" placeholder="b" />
-
+      <input
+        type="text"
+        placeholder="a"
+        value={a}
+        onChange={(event) => setA(Number(event.target.value))}
+      />
+      <input
+        type="text"
+        placeholder="b"
+        value={b}
+        onChange={(event) => setB(Number(event.target.value))}
+      />
       <button
         onClick={() => {
-          dispatch(sum(6, 4));
+          dispatch(sum(a, b));
         }}
       >
         Somar
       </button>
-      <button>Subtrair</button>
+      <button
+        onClick={() => {
+          dispatch(subtract(a, b));
+        }}
+      >
+        Subtrair
+      </button>
 
       <div>{result}</div>
     </>
